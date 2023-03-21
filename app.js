4070c83ff559f4b1804a8baebe73f91bd3e20aad
Pauline Vos <pv@daedalean.ai>
2023-03-21 22:44:03 +0100

    Add graceful shutdown

--- app.js
+++ app.js
@@ -113,6 +113,29 @@ app.post('/user-tokens', auth, (req, res) => {
   res.send(fetchTokens(tokens))
 })
 
-app.listen(port, () => {
-  console.log(`Test server listening on port ${port}`)
-})
+const server = app.listen(port, () => console.log(`Test server running on port ${port}`))
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+server.on('connection', connection => {
+  connections.push(connection);
+  connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+  console.log('Shutting down gracefully');
+  server.close(() => {
+    console.log('Closed out remaining connections');
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+
+  connections.forEach(curr => curr.end());
+  setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
